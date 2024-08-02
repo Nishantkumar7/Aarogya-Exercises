71ad0c9f448cef6279b8821c9328d127965f8dae
Nishantkumar7 <niskum2002@gmail.com>
2024-08-02 22:16:05 +0530

    first commit

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import HeroBannerImage from '../assets/images/gym-girl-3.jpg';
+import './RotatingHexagon.css'
+
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Aarogya Club</Typography>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+      Sweat, Shine <br />
+      And Keep Going
+    </Typography>
+    <Typography fontSize="22px" className='caudex-regular' lineHeight="35px">
+      Check out the most effective exercises personalized to you
+    </Typography>
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
+    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+      Aarogya
+    </Typography>
+    <img src={HeroBannerImage} style={{borderRadius:'0 0 0 100px'}} alt="hero-banner" className="hero-banner-img" />
+  </Box>
+);
+
+export default HeroBanner;
